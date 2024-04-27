e3a43a2a273d670c42585d95f23beb9c8834c860
Joe Goodman <joe@Joes-Mac.local>
2024-04-27 19:40:37 -0400

    Quartz sync: Apr 27, 2024, 7:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "notes.joesmusings.com",
     ignorePatterns: ["private", "templates", ".obsidian", "**/calendar", "**/-/templates", "**/-/daily-notes", "**/planning"],
     defaultDateType: "created",
     theme: {
