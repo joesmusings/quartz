241f477ab07893a509a1f09fa28315ac6892120d
Joe Goodman <joe@Joes-Mac.local>
2024-04-27 20:15:16 -0400

    Quartz sync: Apr 27, 2024, 8:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -43,7 +43,7 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#284b63",
+          secondary: "#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
